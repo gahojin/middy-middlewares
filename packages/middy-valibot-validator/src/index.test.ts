a79191118977ac583881d2154058a09b9ae56c8f
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2024-07-31 15:13:39 +0900

    chore: initial commit

---
+++ packages/middy-valibot-validator/src/index.test.ts
@@ -0,0 +1,345 @@
+import validatorMiddleware from '@/index'
+import middy from '@middy/core'
+import '@valibot/i18n/ja'
+import * as v from 'valibot'
+
+const context = {
+  getRemainingTimeInMillis: () => 1000,
+  callbackWaitsForEmptyEventLoop: true,
+  functionVersion: '$LATEST',
+  functionName: 'lambda',
+  memoryLimitInMB: '128',
+  logGroupName: '/aws/lambda/lambda',
+  logStreamName: 'yyyy/mm/dd/[$LATEST]xxxxxxxxxxxxxxxxxxxxxx',
+  clientContext: undefined,
+  identity: undefined,
+  invokedFunctionArn: 'arn:aws:lambda:ca-central-1:000000000000:function:lambda',
+  awsRequestId: '00000000-0000-0000-0000-0000000000000',
+  done: () => {},
+  fail: () => {},
+  succeed: () => {},
+}
+
+const contextSchema = v.object({
+  getRemainingTimeInMillis: v.custom((input) => typeof input === 'function'),
+  functionVersion: v.string(),
+  functionName: v.string(),
+  invokedFunctionArn: v.string(),
+  memoryLimitInMB: v.string(),
+  awsRequestId: v.string(),
+  logGroupName: v.string(),
+  logStreamName: v.string(),
+  identity: v.optional(
+    v.object({
+      cognitoIdentityId: v.string(),
+      cognitoIdentityPoolId: v.string(),
+    }),
+  ),
+  clientContext: v.optional(
+    v.object({
+      'client.installation_id': v.string(),
+      'client.app_title': v.string(),
+      'client.app_version_name': v.string(),
+      'client.app_version_code': v.string(),
+      'client.app_package_name': v.string(),
+      'env.platform_version': v.string(),
+      'env.platform': v.string(),
+      'env.make': v.string(),
+      'env.model': v.string(),
+      'env.locale': v.string(),
+    }),
+  ),
+  callbackWaitsForEmptyEventLoop: v.boolean(),
+})
+
+describe('validator', () => {
+  it('イベントオブジェクトのバリデーションが動作すること', async () => {
+    const schema = v.object({
+      body: v.object({
+        string: v.string(),
+        boolean: v.pipe(
+          v.string(),
+          v.transform((input) => Boolean(input)),
+        ),
+        integer: v.pipe(
+          v.string(),
+          v.transform((input) => Number(input)),
+          v.integer(),
+        ),
+        number: v.pipe(
+          v.string(),
+          v.transform((input) => Number(input)),
+        ),
+      }),
+    })
+
+    const handler = middy()
+
+    handler.use(
+      validatorMiddleware({
+        eventSchema: schema,
+      }),
+    )
+
+    const event = {
+      body: {
+        string: JSON.stringify({ foo: 'bar' }),
+        boolean: 'true',
+        integer: '0',
+        number: '0.1',
+      },
+    }
+
+    const response = await handler(event, context)
+    expect(response).toBeUndefined()
+  })
+
+  it('イベントオブジェクトのフォーマットがチェックされること', async () => {
+    const schema = v.object({
+      body: v.object({
+        date: v.pipe(v.string(), v.isoDate()),
+        time: v.pipe(v.string(), v.isoTimeSecond()),
+        'date-time': v.pipe(v.string(), v.isoTimestamp()),
+        uri: v.pipe(v.string(), v.url()),
+        email: v.pipe(v.string(), v.email()),
+        ipv4: v.pipe(v.string(), v.ipv4()),
+        ipv6: v.pipe(v.string(), v.ipv6()),
+        uuid: v.pipe(v.string(), v.uuid()),
+      }),
+    })
+
+    const handler = middy()
+
+    const event = {
+      body: {
+        date: '2000-01-01',
+        time: '00:00:00',
+        'date-time': '2000-01-01T00:00:00.000Z',
+        uri: 'https://example.org',
+        email: 'username@example.org',
+        ipv4: '127.0.0.1',
+        ipv6: '2001:0db8:0000:0000:0000:ff00:0042:8329',
+        uuid: '123e4567-e89b-12d3-a456-426614174000',
+      },
+    }
+
+    handler.use(validatorMiddleware({ eventSchema: schema }))
+
+    const response = await handler(event, context)
+    expect(response).toBeUndefined()
+  })
+
+  it('スキーマ不一致の場合、BadRequestとなること', async () => {
+    const schema = v.object({
+      body: v.string(),
+      foo: v.string(),
+    })
+
+    const handler = middy()
+
+    handler.use(validatorMiddleware({ eventSchema: schema }))
+
+    // fooプロパティが存在しない
+    const event = {
+      body: JSON.stringify({ something: 'somethingelse' }),
+    }
+
+    const response = await handler(event, context)
+    expect(response.statusCode).toEqual(400)
+    expect(JSON.parse(response.body)).toEqual({
+      message: 'Event object failed validation',
+      error: [
+        expect.objectContaining({
+          type: 'string',
+          expected: 'string',
+          received: 'undefined',
+          message: 'Invalid type: Expected string but received undefined',
+          path: [expect.objectContaining({ key: 'foo' })],
+          lang: 'en',
+        }),
+      ],
+    })
+  })
+
+  const cases: Record<string, string> = {
+    en: 'Invalid type: Expected string but received undefined',
+    ja: '無効な型: string を期待しましたが、 undefined を受け取りました',
+  }
+
+  it.each(['en', 'ja'])('スキーマ不一致の場合、BadRequestとなること (異なる言語設定) (%s)', async (lang) => {
+    const schema = v.object({
+      body: v.string(),
+      foo: v.string(),
+    })
+
+    const handler = middy()
+
+    handler.use(validatorMiddleware({ eventSchema: schema, language: lang }))
+
+    // fooプロパティが存在しない
+    const event = {
+      body: JSON.stringify({ something: 'somethingelse' }),
+    }
+
+    const response = await handler(event, context)
+    expect(response.statusCode).toEqual(400)
+    expect(JSON.parse(response.body)).toEqual({
+      message: 'Event object failed validation',
+      error: [
+        expect.objectContaining({
+          message: cases[lang],
+          path: [expect.objectContaining({ key: 'foo' })],
+          type: 'string',
+        }),
+      ],
+    })
+  })
+
+  it('スキーマ不一致の場合、BadRequestとなること (言語設定なし)', async () => {
+    const schema = v.object({
+      body: v.string(),
+      foo: v.string(),
+    })
+
+    const handler = middy()
+
+    handler.use(validatorMiddleware({ eventSchema: schema }))
+
+    // fooプロパティが存在しない
+    const event = {
+      body: JSON.stringify({ something: 'somethingelse' }),
+    }
+
+    const response = await handler(event, context)
+    expect(response.statusCode).toEqual(400)
+    expect(JSON.parse(response.body)).toEqual({
+      message: 'Event object failed validation',
+      error: [
+        expect.objectContaining({
+          // valibotのデフォルト言語となる
+          message: 'Invalid type: Expected string but received undefined',
+          path: [expect.objectContaining({ key: 'foo' })],
+          type: 'string',
+        }),
+      ],
+    })
+  })
+
+  it('contextオブジェクトのバリデーションが動作すること', async () => {
+    const handler = middy()
+
+    handler.use(validatorMiddleware({ contextSchema: contextSchema }))
+
+    const response = await handler({}, context)
+    expect(response).toBeUndefined()
+  })
+
+  it('誤ったcontextオブジェクトの場合、BadRequestとなること', async () => {
+    const handler = middy()
+
+    handler
+      .before((request) => {
+        Object.assign(request.context, { callbackWaitsForEmptyEventLoop: 'fail' })
+      })
+      .use(validatorMiddleware({ contextSchema: contextSchema }))
+
+    const response = await handler({}, context)
+    expect(response.statusCode).toEqual(400)
+    expect(JSON.parse(response.body)).toEqual({
+      message: 'Context object failed validation',
+      error: [
+        expect.objectContaining({
+          // valibotのデフォルト言語となる
+          message: 'Invalid type: Expected boolean but received "fail"',
+          path: [expect.objectContaining({ key: 'callbackWaitsForEmptyEventLoop' })],
+          type: 'boolean',
+        }),
+      ],
+    })
+  })
+
+  it('レスポンスオブジェクトのバリデーションが動作すること', async () => {
+    const handler = middy(() => {
+      return {
+        body: 'Hello world',
+        statusCode: 200,
+      }
+    })
+
+    const schema = v.object({
+      body: v.string(),
+      statusCode: v.number(),
+    })
+
+    handler.use(validatorMiddleware({ responseSchema: schema }))
+
+    const response = await handler({}, context)
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual('Hello world')
+  })
+
+  it('誤ったレスポンスオブジェクトの場合、BadRequestとなること', async () => {
+    const schema = v.object({
+      body: v.object({}),
+      statusCode: v.number(),
+    })
+
+    const handler = middy(() => {
+      return {} as v.InferOutput<typeof schema>
+    })
+
+    handler.use(validatorMiddleware({ responseSchema: schema }))
+
+    const response = await handler({}, context)
+    expect(response.statusCode).toEqual(400)
+    expect(JSON.parse(response.body as string)).toEqual({
+      message: 'Response object failed validation',
+      error: [
+        expect.objectContaining({
+          message: 'Invalid type: Expected Object but received undefined',
+          path: [expect.objectContaining({ key: 'body' })],
+          type: 'object',
+        }),
+        expect.objectContaining({
+          message: 'Invalid type: Expected number but received undefined',
+          path: [expect.objectContaining({ key: 'statusCode' })],
+          type: 'number',
+        }),
+      ],
+    })
+  })
+
+  it('誤ったメールフォーマットは許可されないこと', async () => {
+    const schema = v.object({
+      email: v.pipe(v.string(), v.email()),
+    })
+
+    const handler = middy()
+
+    handler.use(validatorMiddleware({ eventSchema: schema }))
+
+    const response = await handler({ email: 'abc@abc' }, context)
+    expect(response.statusCode).toEqual(400)
+    expect(JSON.parse(response.body as string)).toEqual({
+      message: 'Event object failed validation',
+      error: [
+        expect.objectContaining({
+          message: 'Invalid email: Received "abc@abc"',
+          path: [expect.objectContaining({ key: 'email' })],
+          type: 'email',
+        }),
+      ],
+    })
+  })
+
+  it('配列のバリデーションが動作すること', async () => {
+    const schema = v.union([v.object({ test: v.string() }), v.array(v.object({ test: v.string() }))])
+
+    const handler = middy()
+
+    handler.use(validatorMiddleware({ eventSchema: schema }))
+
+    await handler({ test: 'test' }, context)
+    await handler([{ test: 'test' }, { test: 'test2' }, { test: 'test3' }], context)
+  })
+})
---
+++ packages/middy-valibot-validator/src/index.ts
@@ -0,0 +1,70 @@
+import type middy from '@middy/core'
+import * as v from 'valibot'
+import type { GenericSchema } from 'valibot'
+
+type Options = {
+  eventSchema?: GenericSchema
+  contextSchema?: GenericSchema
+  responseSchema?: GenericSchema
+  language?: string
+  errorResponse?: (statusCode: number, message: string, issues?: [v.BaseIssue<unknown>, ...v.BaseIssue<unknown>[]]) => any
+}
+
+const defaults: Options = {
+  language: 'en',
+}
+
+const validatorMiddleware = (options: Options = {}): middy.MiddlewareObj => {
+  const opts = { ...defaults, ...options }
+  const { eventSchema, contextSchema, responseSchema, language } = opts
+
+  const errorResponse =
+    opts.errorResponse ??
+    ((statusCode, message, issues) => {
+      console.error({
+        message,
+        error: JSON.stringify(issues),
+      })
+      return {
+        statusCode,
+        body: JSON.stringify({ message, error: issues }),
+      }
+    })
+
+  const beforeFn: middy.MiddlewareFn = async (request) => {
+    if (eventSchema) {
+      const validEvent = await v.safeParseAsync(eventSchema, request.event, { lang: language })
+      if (validEvent.success) {
+        Object.assign(request.event, validEvent.output)
+      } else {
+        return errorResponse(400, 'Event object failed validation', validEvent.issues)
+      }
+    }
+
+    if (contextSchema) {
+      const validContext = await v.safeParseAsync(contextSchema, request.context, { lang: language })
+      if (validContext.success) {
+        Object.assign(request.context, validContext.output)
+      } else {
+        return errorResponse(400, 'Context object failed validation', validContext.issues)
+      }
+    }
+  }
+
+  const afterFn: middy.MiddlewareFn = async (request) => {
+    if (responseSchema) {
+      const validResponse = await v.safeParseAsync(responseSchema, request.response, { lang: language })
+      if (validResponse.success) {
+        Object.assign(request.response, validResponse.output)
+      } else {
+        return errorResponse(400, 'Response object failed validation', validResponse.issues)
+      }
+    }
+  }
+  return {
+    before: eventSchema ?? contextSchema ? beforeFn : undefined,
+    after: responseSchema ? afterFn : undefined,
+  }
+}
+
+export default validatorMiddleware
