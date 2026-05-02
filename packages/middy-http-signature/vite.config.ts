f0d27f06a8a09232a0a13daf3848d064f628bc4d
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-05-02 21:31:10 +0900

    build: バンドラをrolldownからviteに変更する
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

---
+++ packages/middy-http-signature/vite.config.ts
@@ -0,0 +1,38 @@
+import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [
+    dts({
+      exclude: ['src/**/*.test.ts'],
+    }),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  build: {
+    ssr: true,
+    lib: {
+      entry: ['src/index.ts'],
+      formats: ['es'],
+    },
+    minify: false,
+    sourcemap: true,
+    rolldownOptions: {
+      treeshake: true,
+      output: {
+        cleanDir: true,
+        comments: false,
+        preserveModules: true,
+      },
+      platform: 'node',
+      external: [/^node:/, /^@middy\//, 'aws-lambda'],
+      optimization: {
+        inlineConst: { mode: 'all', pass: 5 },
+      },
+      experimental: {
+        nativeMagicString: true,
+      },
+    },
+  },
+})
